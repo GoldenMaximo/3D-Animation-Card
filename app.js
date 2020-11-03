5879eb1103e97da82424b5a0bb9b6e11c86dd972
Gustavo Máximo <gfmaximo97@gmail.com>
2020-11-03 02:33:46 -0300

    feat: Make animations mobile responsive

--- app.js
+++ app.js
@@ -7,17 +7,28 @@ const purchase = document.querySelector('.purchase button');
 const description = document.querySelector('.info h3');
 const sizes = document.querySelector('.sizes');
 
+// Multiple event listener function for mobile + desktop events
+const addMultipleEventListeners = (element, events, handler) => {
+    events.forEach(e => element.addEventListener(e, handler))
+}
+
 // Mouse movement animation trigger
-container.addEventListener('mousemove', e => {
+addMultipleEventListeners(container, ['mousemove', 'touchmove'], e => {
     // (window.innerWidth / 2) Gets the whole monitor width then
     //  cuts it in half to arrive straight into the center
-    const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+
+    let touch;
+    if (e.changedTouches) {
+        touch = e.changedTouches[0];
+    }
+
+    const xAxis = (window.innerWidth / 2 - (e.pageX || touch.pageX)) / 25;
+    const yAxis = (window.innerHeight / 2 - (e.pageY || touch.pageY)) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 // Animate In
-container.addEventListener('mouseenter', e => {
+addMultipleEventListeners(container, ['mouseenter', 'touchstart'], () => {
     // If the transition was straight up added to the css file
     //  it would result in the mouse movement transform animation
     //   getting "transitioned" as well
@@ -32,7 +43,7 @@ container.addEventListener('mouseenter', e => {
 });
 
 // Animate Out
-container.addEventListener('mouseleave', e => {
+addMultipleEventListeners(container, ['mouseleave', 'touchend'], () => {
     card.style.transition = 'all 0.5s ease';
     card.style.transform = 'rotateY(0deg) rotateX(0deg)';
 
